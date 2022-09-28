cb5a092e3cbdacbb1433febf0a75ef85956b8fa2
qraxex <samuelpaulyila@gmail.com>
2022-09-28 23:35:04 +0100

    Update code with Path Params

--- test/com/herokuapp/restfulbooker/GetBookingsIDWIthFilterExerciseTest.java
+++ test/com/herokuapp/restfulbooker/GetBookingsIDWIthFilterExerciseTest.java
@@ -9,24 +9,37 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-public class GetBookingsIDWIthFilterExerciseTest {
-	
+public class GetBookingsIDWIthFilterExerciseTest extends BaseTest {
+
 	String URL = "https://restful-booker.herokuapp.com/";
-	//String URL = "https://restful-booker.herokuapp.com/booking?firstname=Abiola&lastname=Lamidi";
+
+	// String URL =
+	// "https://restful-booker.herokuapp.com/booking?firstname=Abiola&lastname=Lamidi";
 	@Test
 	public void getBookingsIDWIthFilterExerciseTest() {
-		
-		RequestSpecification rspec = new RequestSpecBuilder().setBaseUri(URL).build();
-		
-		//get booking ids response for first 10 bookings
-		Response response = RestAssured.given(rspec).get("booking/2382");
+
+		// RequestSpecification rspec = new
+		// RequestSpecBuilder().setBaseUri(URL).build();
+
+		// create the booking
+		Response createResponse = createBookings();
+		createResponse.print();
+
+		// return new booking id
+		int bookingid = createResponse.jsonPath().getInt("bookingid");
+
+		// Improve the code with path params
+		rspec.pathParam("bookingId", bookingid);
+
+		// get booking IDs response for first 10 bookings
+		Response response = RestAssured.given(rspec).get("booking/{bookingId}");
 		response.prettyPrint();
-		
+
 		Assert.assertEquals(response.getStatusCode(), 200, "Status code should return 200!");
-		
+
 		SoftAssert sa = new SoftAssert();
-		
-		//get values of each returned JSon key
+
+		// get values of each returned JSon key
 		String actualFName = response.jsonPath().getString("firstname");
 		String actualLName = response.jsonPath().getString("lastname");
 		int price = response.jsonPath().getInt("totalprice");
@@ -34,17 +47,17 @@ public class GetBookingsIDWIthFilterExerciseTest {
 		String actualCheckin = response.jsonPath().getString("bookingdates.checkin");
 		String actualCheckout = response.jsonPath().getString("bookingdates.checkout");
 		String actualNeeds = response.jsonPath().getString("additionalneeds");
-		
-		//cross check all the fields with expected values 
-		sa.assertEquals(actualFName, "Sally");
-		sa.assertEquals(actualLName, "Brown");
-		sa.assertEquals(price, 111);
+
+		// cross check all the fields with expected values
+		sa.assertEquals(actualFName, "Paul Yila");
+		sa.assertEquals(actualLName, "Samuel");
+		sa.assertEquals(price, 1000);
 		sa.assertTrue(actualDPaid);
-		sa.assertEquals(actualCheckin, "2013-02-23");
-		sa.assertEquals(actualCheckout, "2014-10-23");
-		sa.assertEquals(actualNeeds, "Breakfast");
-		
-		//run all the assertions
+		sa.assertEquals(actualCheckin, "2022-09-26");
+		sa.assertEquals(actualCheckout, "2022-09-30");
+		sa.assertEquals(actualNeeds, "New Whip");
+
+		// run all the assertions
 		sa.assertAll();
 	}
 
